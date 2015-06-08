7cdd0d1f3c9ba1427a23764a7d866909291a6e19
Chris Sefkin <c.sefkin@gmail.com>
2015-06-08 15:42:25 -0700

    so ugly and definitely not fully functional

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 Bustle.Router.map(function() {
   this.resource('homepage', {path: '/'});
   this.resource('story1');
+  this.resource('story2');
+  this.resource('story3');
+  this.resource('story4');
 });
